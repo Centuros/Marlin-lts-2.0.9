80faf936410c3ab59907e5cbf91da6cd71caf586
Phil Levinson <69514526+Centuros@users.noreply.github.com>
2024-01-10 08:49:36 -0500

    Inverted X direction is true v1.3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -136,7 +136,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "SWX1 BMG E3DV6 v1.2"
+#define CUSTOM_MACHINE_NAME "SWX1 BMG E3DV6 v1.3"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -1544,7 +1544,7 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
+#define INVERT_X_DIR true
 #define INVERT_Y_DIR false
 #define INVERT_Z_DIR false
 //#define INVERT_I_DIR false
