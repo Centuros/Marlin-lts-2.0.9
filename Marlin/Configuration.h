723844d20fc691eac916540911e5a413a79fc717
Phil Levinson <69514526+Centuros@users.noreply.github.com>
2024-01-12 12:00:57 -0500

    v3.6

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -122,7 +122,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "SWX1 BMG E3DV6 v3.5"
+#define CUSTOM_MACHINE_NAME "SWX1 BMG E3DV6 v3.6"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
